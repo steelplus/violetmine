cc9a25e13bb9d69f0f16a3ccbad604a25e899d90
nagaoka.aya <nagaoka.aya@intra.vpjr.jp>
2018-03-14 12:05:56 +0900

    新規 ダイアログコンポーネント

---
+++ components/common/RuDialog.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+
+        <header class="modal-card-head">
+          <slot name="header">
+            default header
+          </slot>
+          <button @click="$emit('close')" class="delete" aria-label="close"></button>
+        </header>
+
+        <section class="modal-card-body">
+          <slot>
+            default body
+          </slot>
+        </section>
+
+        <footer class="modal-card-foot">
+          <slot name="footer">
+            default footer
+          </slot>
+          <button @click="$emit('close')" class="button">キャンセル</button>
+        </footer>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RuDialog',
+  };
+</script>
+
+<style scoped lang="scss">
+  .modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 640px;
+  margin: 0px auto;
+  padding: 0px 0px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header {
+  text-align: left;
+}
+
+.modal-body {
+  text-align: center;
+}
+
+.modal-default-button {
+  float: right;
+}
+</style>
